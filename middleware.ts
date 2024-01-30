bba67eaadbf9eb744db7bb086f52b75df37cd9f7
Jorge Matos <jisapjisap@gmail.com>
2024-01-30 18:18:14 +0100

    middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { auth } from "./auth"
+
+export default auth((req) => {
+
+  const isLoggedIn = !!req.auth;
+
+  console.log("ROUTE:",  req.nextUrl.pathname);
+  console.log("IS LOGGEDIN", isLoggedIn);
+})
+
+// Todas las rutas que coincidan con el matcher pasaran por auth, con excepción de los archivos estáticos de next
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
