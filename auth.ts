bc591ced22a92dae1d7aa578085d39c3b1a6b051
Jorge Matos <jisapjisap@gmail.com>
2024-01-30 18:51:29 +0100

    configuration auth & adapaters

--- auth.ts
+++ auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import authConfig from "@/auth.config"
+import { PrismaAdapter } from "@auth/prisma-adapter" 
+import { db } from "./lib/db"
 
 export const { handlers: { GET, POST }, auth,} = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(db),   // bd que se va a utilizar
+  session: { strategy: "jwt" }, // estrategía de autenticación
+  ...authConfig,                // proveedor de autenticación    
 })
