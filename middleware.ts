30f7279dc07cc8c25a0c1c282a7abcb466b2872b
Jorge Matos <jisapjisap@gmail.com>
2024-01-31 19:35:38 +0100

    rutas públicas y protegidas

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,33 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes, } from "./route";
 
-const { auth } = NextAuth(authConfig)
-
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;  
 
-  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  console.log("ROUTE:",  req.nextUrl.pathname);
-  console.log("IS LOGGEDIN", isLoggedIn);
+  if(isApiAuthRoute){ // En estas rutas de autenticación del server no se deja entrar
+    return null
+  };
+
+  if(isAuthRoute){                                                        // Si entramos en una ruta que requiere autenticación (/auth/login o register)
+    if(isLoggedIn){                                                       // comprobamos si el usuario está logueado 
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl)); // y si lo está redirect a /settings
+    }
+    return null                                                           // Si no esta logueado return null y entramos a esas rutas de logueo o register
+  };
+
+  if(!isLoggedIn && !isPublicRoute){                                      // Si el usuario no esta logueado y no es una ruta pública
+    return Response.redirect(new URL("/auth/login", nextUrl));            // redirect para que loguee
+  }
+
+  return null;
 })
 
 // Todas las rutas que coincidan con el matcher pasaran por auth, con excepción de los archivos estáticos de next
---
+++ route.ts
@@ -0,0 +1,31 @@
+/**
+ * Array de rutas accesibles al público que 
+ * no requieren autenticación
+ * @type {string[]}
+ */
+export const publicRoutes =  [
+  "/"
+];
+
+/**
+ * Array de rutas que requieren de autenticación
+ * Estas rutas redirijiran a los usuarios logueados a /settings
+ * @type {string[]}
+ */
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register",
+];
+
+/**
+ * Prefijo para las rutas API de autenticación
+ * Todas las rutas que empiecen con este prefijo serán usadas con propositos de autenticación
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/** 
+ * Redireccionamiento por defecto despues del logging in
+ * @type {string}
+*/
+export const DEFAULT_LOGIN_REDIRECT = "settings";
