9d978c8e9bf93a34a326a8b3873ceed88ca8b18d
Jorge Matos <jisapjisap@gmail.com>
2024-02-07 19:06:41 +0100

    generateTwoFactorToken

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,34 @@ import { getVerificationTokenByEmail } from '@/data/verification-token';
 import { v4 as uuidv4 } from 'uuid'
 import { db } from './db';
 import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
+import crypto from 'crypto'
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+
+export const generateTwoFactorToken = async (email: string) => {
+
+  const token = crypto.randomInt(100_000, 1_000_000).toString();    // Crea token
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);   // Crea fecha de expiración
+
+  const existingToken = await getTwoFactorTokenByEmail(email);      // Busca el objeto twoFactorToken en su table según email
+
+  if (existingToken) {                                              // Si existe lo borra
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      }
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({           // Crea un nuevo objeto twoFactorToken con los nuevos datos
+    data: {
+      email,
+      token,
+      expires,
+    }
+  });
+
+  return twoFactorToken;
+}
 
 export const generateVerificationToken = async (email:string) => {
 
